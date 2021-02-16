ccf2c12acdb96bd92c53d1be47b1d2da7928c801
Finn AM <mahalli500@gmail.com>
2021-02-16 10:24:09 +0700

    mobile almost done

---
+++ app.js
@@ -0,0 +1,22 @@
+const testimoniArr = [
+{id:1,
+name: 'Mom',
+img: '/img/mom.jpg',
+message : `I Don't have a clue but i'll support him`},
+{id:2,
+name: 'Dad',
+img: '/img/dad.jpg',
+message : `IDGAF, but don't forget me after you got the salary`},
+{id:3,
+name: 'Big bro',
+img: '/img/bro.jpg',
+message : `Stop doing this and join my company`},
+{id:4,
+name: 'Lil sis',
+img: '/img/lil_sis.jpg',
+message : `I thought programmer is cool`},
+{id:5,
+name: 'My Girlfriend',
+img: '/img/girlfriend.jpg',
+message : `Why you include me? i'm only exist in your mind!`
+}];
