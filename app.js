f6950f4c12cea8449b5b72a604c9cc135bab3f75
Finn Ahamed <irwansumenep500@gmail.com>
2021-02-17 16:57:18 +0700

    adding tabs,slider,and hamburger menu

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Testimoni start
+
 const testimoniArr = [
 {id:1,
 name: 'Mom',
@@ -23,8 +25,84 @@ message : `Why you include me? i'm only exist in your mind!`
 
 const left = document.querySelector('.left')
 const right = document.querySelector('.right')
+let names = document.querySelector('.testi-name')
+let message = document.querySelector('.testi-message')
+let image = document.querySelector('.testi-img')
+counter = 0;
+
+
+right.addEventListener('click' , ()=>{
+counter++
+if(counter >= testimoniArr.length ){
+ counter = 0;
+}
+slider();
+})
 
 left.addEventListener('click' , ()=>{
- alert('wtf dude')
+counter--
+if(counter < 0 ){
+ counter = testimoniArr.length - 1;
+}
+slider();
 
 })
+
+
+function slider(){
+
+names.textContent = testimoniArr[counter].name
+message.textContent = testimoniArr[counter].message
+image.style.backgroundImage = "url" + `(${testimoniArr[counter].img})`
+
+console.log(counter);
+}
+// Testimoni End
+
+// Tabs start
+
+const container = document.querySelector('.containers');
+const btns = document.querySelectorAll('.btn');
+const section = document.querySelectorAll('.sect');
+
+container.addEventListener('click',function(e){
+
+ let id = e.target.dataset.id;
+
+if (id){
+
+btns.forEach((btn) =>{
+ btn.classList.remove('active')
+ e.target.classList.add('active')
+})
+
+section.forEach((sect) =>{
+ sect.classList.remove('active')
+ sect.classList.remove('act-flex')
+})
+
+const element = document.getElementById(id)
+console.log(element.id);
+
+
+// making diffrence in flex / block
+
+if(element.id === 'testi'){
+ element.classList.add('act-flex')
+}else{
+element.classList.add('active')}
+
+} })
+
+// hamburger menu
+
+const burger = document.querySelector('.hamburger')
+const navbar = document.querySelector('.navbar')
+const nav = document.querySelector('.nav-container')
+
+burger.addEventListener('click', ()=>{
+burger.classList.toggle('show')
+navbar.classList.toggle('show')
+navbar.classList.add('anim')
+})
+
