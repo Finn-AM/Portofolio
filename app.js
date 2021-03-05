74e3a4f516a07adfa2de38654961fe9afce862e7
Finn Ahamed <irwansumenep500@gmail.com>
2021-03-05 11:22:06 +0700

    add+1 project

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 // Testimoni start
 
 const testimoniArr = [
-{id:1,
-name: '~Mom',
-img: './img/mom.jpg',
-message : `"I Don't have a clue but i'll support him "`},
-{id:2,
-name: '~Dad',
-img: './img/dad.jpg',
-message : `"IDGAF, but don't forget me after you got the salary"`},
-{id:3,
-name: '~Big bro',
-img: './img/bro.jpg',
-message : `"Stop doing this and join my company"`},
-{id:4,
-name: '~Lil sis',
-img: './img/lil_sis.jpg',
-message : `"I thought programmer is cool"`},
-{id:5,
-name: '~My Girlfriend',
-img: './img/girlfriend.jpg',
-message : `"Why you include me? i'm only exist in your mind!"`
-}];
+  {
+    id: 1,
+    name: '~Mom',
+    img: './img/mom.jpg',
+    message: `"I Don't have a clue but i'll support him "`,
+  },
+  {
+    id: 2,
+    name: '~Dad',
+    img: './img/dad.jpg',
+    message: `"IDGAF, but don't forget me after you got the salary"`,
+  },
+  {
+    id: 3,
+    name: '~Big bro',
+    img: './img/bro.jpg',
+    message: `"Stop doing this and join my company"`,
+  },
+  {
+    id: 4,
+    name: '~Lil sis',
+    img: './img/lil_sis.jpg',
+    message: `"I thought programmer is cool"`,
+  },
+  {
+    id: 5,
+    name: '~My Girlfriend',
+    img: './img/girlfriend.jpg',
+    message: `"Why you include me? i'm only exist in your mind!"`,
+  },
+];
 
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
@@ -30,37 +40,42 @@ let message = document.querySelector('.testi-message');
 let image = document.querySelector('.testi-img');
 counter = 0;
 
-
-right.addEventListener('click' , ()=>{
+right.addEventListener('click', () => {
   sliderAnimation();
-  counter++
-if(counter >= testimoniArr.length ){
- counter = 0;
-}
-slider();
-})
+  counter++;
+  if (counter >= testimoniArr.length) {
+    counter = 0;
+  }
+  slider();
+});
 
-left.addEventListener('click' , ()=>{
+left.addEventListener('click', () => {
   sliderAnimation();
-counter--
-if(counter < 0 ){
- counter = testimoniArr.length - 1;
+  counter--;
+  if (counter < 0) {
+    counter = testimoniArr.length - 1;
+  }
+  slider();
+});
+
+function slider() {
+  names.textContent = testimoniArr[counter].name;
+  message.textContent = testimoniArr[counter].message;
+  image.style.backgroundImage = 'url' + `(${testimoniArr[counter].img})`;
+
+  console.log(counter);
 }
-slider();
-})
 
-function slider(){
-names.textContent = testimoniArr[counter].name;
-message.textContent = testimoniArr[counter].message;
-image.style.backgroundImage = "url" + `(${testimoniArr[counter].img})`;
-
-console.log(counter);
+function sliderAnimation() {
+  image.animate([{ opacity: '0.1' }, { opacity: '1' }], {
+    duration: 500,
+    fill: 'forwards',
+  });
+  message.animate([{ opacity: '0.1' }, { opacity: '1' }], {
+    duration: 500,
+    fill: 'forwards',
+  });
 }
-
-function sliderAnimation(){
-image.animate([{opacity:'0.1'},{opacity:'1'}],{duration:500,fill:'forwards'});
-message.animate([{opacity:'0.1'},{opacity:'1'}],{duration:500,fill:'forwards'});
-};
 
 // Testimoni End
 
@@ -70,34 +85,36 @@ const container = document.querySelector('.containers');
 const btns = document.querySelectorAll('.btn');
 const section = document.querySelectorAll('.sect');
 
-container.addEventListener('click',function(e){
+container.addEventListener('click', function (e) {
+  let id = e.target.dataset.id;
 
- let id = e.target.dataset.id;
+  if (id) {
+    btns.forEach((btn) => {
+      btn.classList.remove('active');
+      e.target.classList.add('active');
+    });
 
-if (id){
+    section.forEach((sect) => {
+      sect.classList.remove('active');
+      sect.classList.remove('act-flex');
+    });
 
-btns.forEach((btn) =>{
- btn.classList.remove('active');
- e.target.classList.add('active');
+    const element = document.getElementById(id);
+
+    element.animate([{ opacity: '0.1' }, { opacity: '1' }], {
+      duration: 500,
+      fill: 'forwards',
+    });
+
+    // making diffrence in flex / block
+
+    if (element.id === 'testi') {
+      element.classList.add('act-flex');
+    } else {
+      element.classList.add('active');
+    }
+  }
 });
-
-section.forEach((sect) =>{
- sect.classList.remove('active');
- sect.classList.remove('act-flex');
-});
-
-const element = document.getElementById(id);
-
-element.animate([{opacity:'0.1'},{opacity:'1'}],{duration:500,fill:'forwards'});
-
-// making diffrence in flex / block
-
-if(element.id === 'testi'){
- element.classList.add('act-flex')
-}else{
-element.classList.add('active')}
-
-} })
 // Tabs end
 
 // hamburger menu
@@ -106,11 +123,11 @@ const burger = document.querySelector('.hamburger');
 const navbar = document.querySelector('.navbar');
 const nav = document.querySelector('.nav-container');
 
-burger.addEventListener('click', ()=>{
-burger.classList.toggle('show');
-nav.classList.toggle('show');
-navbar.classList.toggle('show');
-navbar.classList.add('anim');
+burger.addEventListener('click', () => {
+  burger.classList.toggle('show');
+  nav.classList.toggle('show');
+  navbar.classList.toggle('show');
+  navbar.classList.add('anim');
 });
 
 // UL value is hard coded, if u wanna add more ul , eg. services/course  etc... just add more height to .nav-container. 1 ul += 8vheight.
@@ -119,10 +136,10 @@ navbar.classList.add('anim');
 
 window.addEventListener('resize', removeShow);
 
-function removeShow(){
-  let minWidth = window.innerWidth = 700;
+function removeShow() {
+  let minWidth = (window.innerWidth = 700);
   if (minWidth) {
-   burger.classList.remove('show')
+    burger.classList.remove('show');
     navbar.classList.remove('show');
     nav.classList.remove('show');
   }
@@ -130,26 +147,25 @@ function removeShow(){
 
 // Adding Fixed navbar when passing intro section
 
-const introSect = document.querySelector('.header_intro')
-
-
+const introSect = document.querySelector('.header_intro');
 
 window.addEventListener('scroll', addFixed);
 
 function addFixed() {
- let target = introSect.getBoundingClientRect().bottom;
-let current = window.scrollY;
+  let target = introSect.getBoundingClientRect().bottom;
+  let current = window.scrollY;
 
-if (current >= target){
- nav.classList.add('fixed')}
-else{
- nav.classList.remove('fixed')}
-};
+  if (current >= target) {
+    nav.classList.add('fixed');
+  } else {
+    nav.classList.remove('fixed');
+  }
+}
 
 // preloader
 
 const preloader = document.querySelector('.preloader');
 
-window.addEventListener('load', ()=>{
-  preloader.classList.add('hide')
-})
+window.addEventListener('load', () => {
+  preloader.classList.add('hide');
+});
